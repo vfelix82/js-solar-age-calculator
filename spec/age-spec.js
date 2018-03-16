aed1de5c9bc232102337afaef31632566dc80525
Victor Felix <vfelix82@gmail.com>
2018-03-16 15:11:10 -0700

    add test for life expectancy for mercury, venus, mars, and jupiter.

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,1 +1,29 @@
+import { Age } from './../src/age.js';
 
+describe ('Age', function(){
+  it('should convert Earth age to age on Mercury', function() {
+    let age = new Age(35);
+    expect(age.ageOnMercury(35)).toEqual(8);
+  });
+});
+
+describe ('Age', function(){
+  it('should convert Earth age to age on Venus', function() {
+    let age = new Age(35);
+    expect(age.ageOnVenus(35)).toEqual(21.7);
+  });
+});
+
+describe ('Age', function(){
+  it('should convert Earth age to age on Mars', function() {
+    let age = new Age(35);
+    expect(age.ageOnMars(35)).toEqual(65.8);
+  });
+});
+
+describe ('Age', function(){
+  it('should convert Earth age to age on Jupiter', function() {
+    let age = new Age(35);
+    expect(age.ageOnJupiter(35)).toEqual(415.09999999999997);
+  });
+});
---
+++ src/age.js
@@ -0,0 +1,64 @@
+export class Age {
+
+  constructor(age)
+  {
+    this.age = age;
+  }
+
+  inSeconds(age)
+  {
+    let ageInSeconds = age * 31536000;
+    return ageInSeconds;
+  }
+
+  ageOnMercury(age)
+  {
+    let ageOnMercury = age * 0.24;
+    return Math.round(ageOnMercury);
+  }
+
+  ageOnVenus(age)
+  {
+    let ageOnVenus = age * 0.62;
+    return ageOnVenus;
+  }
+
+  ageOnMars(age)
+  {
+    let ageOnMars = age *1.88;
+    return ageOnMars;
+  }
+
+  ageOnJupiter(age)
+  {
+    let ageOnJupiter = age *11.86;
+    return ageOnJupiter;
+  }
+
+  lifeExpectancyOnMercury(age) {
+    let thisAge = new Age(age);
+    let lifeExpectancyOnMercury = thisAge.ageOnMercury(age);
+    return Math.round(lifeExpectancyOnMercury);
+  }
+
+  lifeExpectancyOnVenus(age) {
+    let thisAge = new Age(age);
+    let lifeExpectancyOnVenus = thisAge.ageOnVenus(age);
+    return Math.round(lifeExpectancyOnVenus);
+  }
+
+  lifeExpectancyOnMars(age)
+  {
+    let thisAge = new Age(age);
+    let lifeExpectancyOnMars = thisAge.ageOnMars(age);
+    return Math.round(lifeExpectancyOnMars);
+  }
+
+  lifeExpectancyOnJupiter(age)
+  {
+    let thisAge = new Age(age);
+    let lifeExpectancyOnJupiter = thisAge.ageOnJupiter(age);
+    return Math.round(lifeExpectancyOnJupiter);
+  }
+
+}
